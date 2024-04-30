9642a6d860110f254e4add3fc1b2ba7fb2738ba8
Avatar9054 <lucasrp.armond@gmail.com>
2024-04-29 23:26:03 -0300

    Update

--- Challenges/Cipher_Challenges/Question_3.java
+++ Challenges/Cipher_Challenges/Question_3.java
@@ -1,5 +1,4 @@
-
-
+import java.util.Scanner;
 
 public class Question_3 {
 
@@ -8,7 +7,18 @@ public class Question_3 {
 
 
 
+    public static String getWord() {
+        String word;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Insert the coded phrase:");
+        word = scanner.nextLine(); // Lê a frase cifrada fornecida pelo usuário
+        System.out.println("Press Enter to continue...");
+        scanner.nextLine(); // Aguarda a entrada do usuário
+        System.out.println();
+
+        return word; // Retorna a frase cifrada
+    }
 
     public static void main(String[] args) {
 
